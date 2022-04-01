076eb7eb8afbf2de8a5e495a0d66e4961b59af77
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-03-31 20:49:42 -0400

    more visible

--- main.js
+++ main.js
@@ -77,7 +77,6 @@ function onWindowResize() {
 
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
 window.addEventListener( 'resize', onWindowResize );
